e4378899b439ce1ecfc416b89b1f928f846f8283
dhairya-07 <dhairya3208@gmail.com>
2023-08-13 23:57:53 +0530

    error controller done

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,64 @@
+const AppError = require('../utils/AppError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue;
+  const message = `Duplicate value for the ${Object.keys(value)}: "${
+    value.name
+  }". Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  console.log(err.errors);
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational Error
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // Generic Error
+  return res.status(err.statusCode).json({
+    status: 'Error',
+    message: 'Something wen very wrong!',
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'Error';
-  return res
-    .status(err.statusCode)
-    .json({ status: err.status, msg: err.message });
+
+  if (process.env.NODE_ENV.trim() === 'development') {
+    sendErrorDev(err, res);
+  }
+  if (process.env.NODE_ENV.trim() === 'production') {
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    sendErrorProd(error, res);
+  }
 };
